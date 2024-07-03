56bad0b9f4c995235df9327c23a9f94d72b4cf9b
PropanBen <35637138+PropanBen@users.noreply.github.com>
2024-07-03 11:20:20 +0200

    Bluepad32 + PS4 Controller + New Mow Engine Driver

--- LawnMower.ino
+++ LawnMower.ino
@@ -1,5 +1,5 @@
-#include <Ps3Controller.h>
-#include <EEPROM.h>
+#include <Bluepad32.h>
+
 // Mac Adresse ESP B0:A7:32:D7:80:B8
 
 //Get Mac Adress of ESP32
@@ -20,9 +20,16 @@
 #define Bin1 5
 #define Bin2 4
 
+/*
 #define PMWC 23 //27 
 #define Cin1 16 // 34
 #define Cin2 17 //35
+*/
+
+#define RPWM_PIN 23 
+#define LPWM_PIN 16 
+#define R_EN_PIN 17 
+#define L_EN_PIN 2 
 
 #define PMWD 26
 #define Din1 12
@@ -32,14 +39,10 @@
 #define Ein1 32
 #define Ein2 33
 
-
-
+#define MowSpeed 255
 #define MotorSpeed 255
-int MowSpeed = EEPROM.read(0);
-
-
 bool motorCForward = false;
-
+bool moving = false;
 
 void setupMotorControl() {
   pinMode(PMWA, OUTPUT);
@@ -50,9 +53,18 @@ void setupMotorControl() {
   pinMode(Bin1, OUTPUT);
   pinMode(Bin2, OUTPUT);
   
+  /*
   pinMode(PMWC, OUTPUT);
   pinMode(Cin1, OUTPUT);
   pinMode(Cin2, OUTPUT);
+ */
+  pinMode(RPWM_PIN, OUTPUT);
+  pinMode(LPWM_PIN, OUTPUT);
+  pinMode(R_EN_PIN, OUTPUT);
+  pinMode(L_EN_PIN, OUTPUT);
+
+  digitalWrite(R_EN_PIN, HIGH);
+  digitalWrite(L_EN_PIN, HIGH);
 
   pinMode(PMWD, OUTPUT);
   pinMode(Din1, OUTPUT);
@@ -61,6 +73,8 @@ void setupMotorControl() {
   pinMode(PMWE, OUTPUT);
   pinMode(Ein1, OUTPUT);
   pinMode(Ein2, OUTPUT);
+
+
 }
 
 // Function to move motor A forward
@@ -107,23 +121,46 @@ void brakeMotorB() {
 
 // Function to move motor C forward
 void moveMotorCForward() {
+
+  /*
   digitalWrite(Cin1, HIGH);
   digitalWrite(Cin2, LOW);
   analogWrite(PMWC, MowSpeed);
+  */
+for (int speed = 0; speed <= MowSpeed; speed++) {
+    analogWrite(RPWM_PIN, speed);
+    analogWrite(LPWM_PIN, 0);
+    delay(50); 
+  }
+
 }
 
 // Function to move motor C backward
 void moveMotorCBackward() {
+
+  /*
   digitalWrite(Cin1, LOW);
   digitalWrite(Cin2, HIGH);
   analogWrite(PMWC, MowSpeed);
+  */
+for (int speed = 0; speed <= MowSpeed; speed++) {
+    analogWrite(RPWM_PIN, 0);
+    analogWrite(LPWM_PIN, speed);
+    delay(50); 
+  }
+
 }
 
 // Function to brake motor C
 void brakeMotorC() {
+
+  /*
   digitalWrite(Cin1, HIGH);
   digitalWrite(Cin2, HIGH);
   analogWrite(PMWC, 0);
+  */
+  analogWrite(RPWM_PIN, 0);
+  analogWrite(LPWM_PIN, 0);
 }
 
 // Function to move motor D forward
@@ -182,160 +219,108 @@ void toggleMotorCForward() {
   }
 }
 
-void AddMowSpeed(int speed) {
-  if (speed<= 255) {
-     MowSpeed = (speed + 10);
-  }
-  if (speed >= 255) {
-    speed = 255;
-      MowSpeed = speed;
-  }
-  analogWrite(PMWC, MowSpeed);
+
+
+
+
+ControllerPtr myController = nullptr;
+
+// This callback gets called any time a new gamepad is connected.
+void onConnectedController(ControllerPtr ctl) {
+    if (myController == nullptr) {
+        Serial.println("CALLBACK: Controller is connected");
+        ControllerProperties properties = ctl->getProperties();
+        Serial.printf("Controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName().c_str(), properties.vendor_id, properties.product_id);
+        myController = ctl;
+    } else {
+        Serial.println("CALLBACK: Controller connected, but already in use");
+    }
 }
 
-void RemoveMowSpeed(int speed) {
-  if (speed<= 255) {
-     MowSpeed = (speed - 10);
-  }
-  if (speed <= 10) {
-    speed = 10;
-      MowSpeed = speed;
-  }
-  analogWrite(PMWC, MowSpeed);
+void onDisconnectedController(ControllerPtr ctl) {
+    if (myController == ctl) {
+        Serial.println("CALLBACK: Controller disconnected");
+        myController = nullptr;
+    } else {
+        Serial.println("CALLBACK: Controller disconnected, but not the active one");
+    }
 }
 
-void SaveMowSpeed()
-{
-  EEPROM.write(0, MowSpeed);
-}
-
-
-
-void onConnect(){
-    Serial.println("Connected.");
-}
-
-void notify()
-{
-
- 
-   //Circle Button Acclerate
-   if( abs(Ps3.event.analog_changed.button.circle) ){
-       moveMotorAForward();
-       moveMotorBForward();
-       moveMotorDForward();
-       moveMotorEForward();
-   }
-
-  if( Ps3.event.button_up.circle )
-  {
-       brakeMotorA();
-       brakeMotorB();
-       brakeMotorD();
-       brakeMotorE();
-  }
-
-  // Cross Button Reverse
-  if( abs(Ps3.event.analog_changed.button.cross) ){
-
+void processGamepad(ControllerPtr ctl) {
+    if (ctl->a()) {
+       moving = true;
+       Serial.println("Button X");
        moveMotorABackward();
        moveMotorBBackward();
        moveMotorDBackward();
        moveMotorEBackward();
-   }
+    }
+    if (ctl->b()) {
+      moving = true;
+       moveMotorAForward();
+       moveMotorBForward();
+       moveMotorDForward();
+       moveMotorEForward();
+    } 
+      
+    
+    if (ctl->y()) {
+        Serial.println("Button Dreieck");
+         toggleMotorCForward();
 
-  if( Ps3.event.button_up.cross )
-  {
+    }
+    if (ctl->x()) {
+        Serial.println("Button Viereck");
        brakeMotorA();
        brakeMotorB();
        brakeMotorD();
        brakeMotorE();
-  }
-
-  //Start Mower
-  if( Ps3.event.button_up.triangle ){
-       toggleMotorCForward();
-   }
-
-   if( Ps3.event.button_up.square )
-   {
-    SaveMowSpeed();
-   }
-
-   if( Ps3.event.button_up.r1 )
-   {
-       AddMowSpeed(MowSpeed);
-   }
-    if( Ps3.event.button_up.l1 )
-   {
-       RemoveMowSpeed(MowSpeed);
-   }
-
-// Motor Schema
-//     A ----- B
-//       | C |     
-//     D ----- E
-
- 
-
-      //---------------- Analog stick value events ---------------
-   if( abs(Ps3.event.analog_changed.stick.lx) + abs(Ps3.event.analog_changed.stick.ly) > 10 ){
-
-    if (Ps3.data.analog.stick.lx > 100) {
-        moveMotorAForward();
-        moveMotorDForward();
-        moveMotorBBackward();
-        moveMotorEBackward();
-
     }
-    else if (Ps3.data.analog.stick.lx < -100) {
+
+    int16_t axisX = ctl->axisX();
+    Serial.printf("Left Axis X: %d\n", axisX);
+
+    if (axisX > 500) {
+        Serial.println("Rechts");
+        moving = true;
         moveMotorBForward();
         moveMotorEForward();
         moveMotorABackward();
         moveMotorDBackward();
+
+    } else if (axisX < -500) {
+         moving = true;
+        Serial.println("Links");
+        moveMotorAForward();
+        moveMotorDForward();
+        moveMotorBBackward();
+        moveMotorEBackward();
     }
-    else 
-    {
+
+       if(moving == false)
+       {
        brakeMotorA();
        brakeMotorB();
        brakeMotorD();
        brakeMotorE();
-    }
-
-
-
-       Serial.println(" x="); Serial.print(Ps3.data.analog.stick.lx, DEC);
-       Serial.println(" y="); Serial.print(Ps3.data.analog.stick.ly, DEC);
-    
-   }
+       }
 }
-
-
-
-
 
 void setup() {
-
-  if (EEPROM.read(0) == 255) {
-    MowSpeed = 10;
-  } else {
-    MowSpeed = EEPROM.read(0);
-  }
-
     Serial.begin(115200);
-    setupMotorControl();
-    Ps3.attach(notify);
-    Ps3.attachOnConnect(onConnect);
-    Ps3.begin("B0:A7:32:D7:80:B8");
+    setupMotorControl(); 
+    Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+    const uint8_t* addr = BP32.localBdAddress();
+    Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
 
+    BP32.setup(&onConnectedController, &onDisconnectedController);
+    BP32.forgetBluetoothKeys();
+    BP32.enableVirtualDevice(false);
 }
 
-
-
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  Serial.begin(115200);
-
+    if (BP32.update() && myController && myController->isConnected() && myController->hasData() && myController->isGamepad()) {
+        processGamepad(myController);
+    }
+    delay(150);
 }
